77e36677e2e252d25fb5c28ebcb3258c2c80db9d
Erik Bautista Santibanez <me@erikb.dev>
2026-09-13 11:38:43 -0700

    wip

--- Sources/SwiftWAVCore/Timeline.swift
+++ Sources/SwiftWAVCore/Timeline.swift
@@ -5,8 +5,6 @@ public protocol Timeline {
 }
 
 extension Never: Timeline {}
-extension EmptyContent: Timeline {}
 extension TupleContent: Timeline where repeat each T: Timeline {}
 extension _ConditionalContent: Timeline where T: Timeline, S: Timeline {}
 extension _ArrayContent: Timeline where T: Timeline {}
-extension _OptionalContent: Timeline where T: Timeline {}
